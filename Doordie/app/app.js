70aca97c62306bd070eed4bb26d7482d74920814
jessica kilby <jessicakilby@gmail.com>
2017-05-23 21:24:12 -0500

    Tues Doordie

---
+++ Doordie/app/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("Doordie", ["ngRoute"]);
+
+app.config(["$routeProvider", function($routeProvider){
+    $routeProvider.when("/",
+        {
+            templateUrl: "app/Partials/Login.html",
+            controller: "LoginController"
+        })
+        .when("/home",
+        {
+            templateUrl: "app/home.html",
+            controller: "homeController"
+        })
+        .when("/signup",
+        {
+            templateUrl: "app/Partials/SignUp.html",
+            controller: "SignUpController"
+        })
+}])
